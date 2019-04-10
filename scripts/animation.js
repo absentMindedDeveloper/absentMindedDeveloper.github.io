aaf95f946facf8ecb89a265b6878bf3f1b477611
daedalus <absent-minded.developer@gmail.com>
2019-04-10 10:22:24 -0700

    First commit

--- scripts/animation.js
+++ scripts/animation.js
@@ -1,1 +1,31 @@
-console.log("hello world");
+var p = document.getElementById('p');
+var l = document.getElementById('load');
+var loadButton = document.getElementById('loadButton');
+var load = [];
+
+var programCode = function(processingInstance) {
+	with(processingInstance) {
+		size(600, 600);
+		frameRate(30);
+		var mouseIsPressed = false;
+		
+		
+		draw = function () {
+			background(0, 25, 0);
+		};
+		
+		mousePressed = function() {
+			mouseIsPressed = true;
+		};
+		mouseReleased = function() {
+			mouseIsPressed = false;
+		};
+		
+	}
+};
+
+// Get the canvas that ProcessingJS will use
+var canvas = document.getElementById("mycanvas");
+
+// Pass the function to ProcessingJS constructor
+var processingInstance = new Processing(canvas, programCode);
