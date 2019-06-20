5c54e868ef428377a5ddb14586873bf8a6fd55da
daedalus <absentMindedDeveloper>
2019-06-20 16:52:05 -0700

    Changed it into a graduation countdown

---
+++ script.js
@@ -0,0 +1,37 @@
+var graduationDate = new Date("may 29, 2020 18:00:00").getTime();
+
+var calc = setInterval(function() {
+	var cur = new Date().getTime();
+	var t = graduationDate - cur;
+	var days = Math.floor(t / (1000 * 60 * 60 * 24));
+	var hours = Math.floor((t % (1000 * 60 * 60 * 24))/(1000 * 60 * 60));
+	var minutes = Math.floor((t % (1000 * 60 * 60))/(1000 * 60));
+	var seconds = Math.floor((t % (1000 * 60))/1000);
+	var text = {
+		day: ' days, ',
+		hour: ' hours, ',
+		minute: ' minutes, ',
+		second: ' seconds'
+	};
+	if(days === 1) {
+		text.day = ' day, ';
+	} else {
+		text.day = ' days, ';
+	}
+	if(hours === 1) {
+		text.hour = ' hour, ';
+	} else {
+		text.hour = ' hours, ';
+	}
+	if(minutes === 1) {
+		text.minute = ' minute, ';
+	} else {
+		text.minute = ' minutes, ';
+	}
+	if(seconds === 1) {
+		text.second = ' second';
+	} else {
+		text.second = ' seconds';
+	}
+	document.getElementById('count').innerHTML = days + text.day + hours + text.hour + minutes + text.minute + seconds + text.second + "<br>UNTIL GRADUATION";
+}, 1000);
